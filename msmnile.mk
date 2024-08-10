c6e24e08befe2853ea4a703dc59cb0869f9baece
luffitys <banackidaniel@gmail.com>
2024-08-10 15:06:36 +0200

    sm8150-common: Build missing WFD libs
    Co-authored-by: Edwin Moquete <edwinmmoquete@gmail.com>
    Change-Id: I2cd360cc7fc1260e99111f18f09fefb8231ce01f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -536,9 +536,11 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
+    android.media.audio.common.types-V2-cpp \
     libnl \
     libpng.vendor \
-    libwfdaac_vendor
+    libwfdaac_vendor \
+    vendor.qti.hardware.display.config-V5-ndk
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
